9e89e614c39d0d0a8074daff87359ff2b56878b4
RyanCocking <ry.ctbc@gmail.com>
2018-10-29 20:52:36 +0000

    Added edge checking+direction reversal function that doesn't work. I think tumbling can cause cells near the box edge to 'skip' over and keep going.

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import numpy as np
 
 class System:
     
-    max_time = 100
+    max_time = 100000
     step_size = 1
     total_steps = int(max_time / step_size)
     timesteps = np.linspace(0,max_time,num=total_steps)  # seconds
     
-    np.random.seed(100)
+    seed = 100
+    np.random.seed(seed)
     pi = np.pi
     
-    box_size = 400  # micrometres
+    box_size = 1000  # micrometres
 
     
 def rotation_matrix_x(angle):
@@ -52,12 +53,29 @@ class Cell:
         self.tumble_chance = tumble_chance
         self.run_mode = run_mode
     
+    def edge_check(self):
+        """Check if a cell has wandered to the edge of the box. If it has,
+        reverse its direction to simulate an elastic collision with the box
+        walls."""
+
+        edge = 0.5*System.box_size - (self.speed * System.step_size)
+    
+        if abs(self.position[0]) >= edge:
+            self.direction[0] = -self.direction[0]
+                                                   
+        if abs(self.position[1]) >= edge:
+            self.direction[1] = -self.direction[1]
+
+        if abs(self.position[2]) >= edge:
+            self.direction[2] = -self.direction[2]
+
     
     def run(self):
         """Move forwards in current direction"""
         self.velocity = self.speed * self.direction
         self.position = self.position + (self.velocity * System.step_size)
-    
+
+
     def tumble(self, max_angle):
         """Rotate randomly about the x, y and z axes"""
         
@@ -75,11 +93,11 @@ class Cell:
         
         random_float = np.random.random()
         if self.run_mode==True:
-            if random_float > self.run_chance:
+            if (random_float > self.run_chance) and (self.tumble_chance > 0):
                 self.run_mode = False
                 
         elif self.run_mode==False:
-            if random_float > self.tumble_chance:
+            if (random_float > self.tumble_chance) and (self.run_chance > 0):
                 self.run_mode = True
     
     def update(self):
@@ -88,14 +106,15 @@ class Cell:
         self.switch_mode()
         
         if self.run_mode == True:
+            #self.edge_check()
             self.run()
         elif self.run_mode == False:
             self.tumble(2*System.pi)
     
-    
+
     
 swimmer = Cell(name='Escherichia coli', position=np.array([0,0,0]), speed=20,
-               direction=np.array([1,0,0]), run_chance=0.3, tumble_chance=0.1,
+               direction=np.array([1,1,0]), run_chance=0.3, tumble_chance=0.05,
                run_mode=True)
 
 positions = []
@@ -107,15 +126,41 @@ for time in System.timesteps:
  
 
 positions = np.array(positions)
+x_pos = positions[:,0]
+y_pos = positions[:,1]
+z_pos = positions[:,2]
+displacement = np.array(np.linalg.norm(positions,ord=None,axis=1))
 
 # plot x,y
-plt.plot(positions[:,0],positions[:,1])
+plt.plot(x_pos,y_pos)
 plt.xlabel('x ($\mu$m)')
 plt.ylabel('y ($\mu$m)')
-plt.xlim(-0.5*System.box_size,0.5*System.box_size)
-plt.ylim(-0.5*System.box_size,0.5*System.box_size)
+plt.xlim(-System.box_size,System.box_size)
+plt.ylim(-System.box_size,System.box_size)
 plt.plot([-0.5*System.box_size,0.5*System.box_size],[0,0],color='k',ls='--',
          lw=0.5)
 plt.plot([0,0],[-0.5*System.box_size,0.5*System.box_size],color='k',ls='--',
          lw=0.5)
 plt.savefig('xy_run_tumble.png')
+plt.close()
+
+# plot t,<r^2>
+plt.plot(System.timesteps,displacement)
+plt.ylabel('|r| ($\mu$m)')
+plt.xlabel('t (s)')
+plt.xlim(0,System.max_time)
+plt.savefig('norm_r_vs_t.png')
+plt.close()
+# plot x,y,z distributions
+plt.hist(np.unique(x_pos), bins='auto', density=False)
+#plt.plot([0,0],[0,1],lw=0.5,ls='--',color='k')
+plt.savefig('x_hist.png')
+plt.close()
+plt.hist(np.unique(y_pos), bins='auto', density=False)
+#plt.plot([0,0],[0,1],lw=0.5,ls='--',color='k')
+plt.savefig('y_hist.png')
+plt.close()
+plt.hist(np.unique(z_pos), bins='auto', density=False)
+#plt.plot([0,0],[0,1],lw=0.5,ls='--',color='k')
+plt.savefig('z_hist.png')
+plt.close()
