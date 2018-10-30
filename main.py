d21ae2d06031d6475b88f8b9b76ca43c620bd3fc
RyanCocking <ry.ctbc@gmail.com>
2018-10-30 17:17:33 +0000

    Separating classes for 1, 2 and 3 dimensions into separate files.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from Cell3D import Cell3D
+from Cell2D import Cell2D
+from Cell1D import Cell1D
+
 class System:
     
-    max_time = 100000
+    max_time = 1000
     step_size = 1
     total_steps = int(max_time / step_size)
     timesteps = np.linspace(0,max_time,num=total_steps)  # seconds
@@ -40,100 +44,35 @@ def rotation_matrix_z(angle):
                      [0,0,1]])
 
     
-class Cell:
-    
-    def __init__(self, name, position, speed, direction, run_chance,
-                 tumble_chance, run_mode):
-        self.name = name
-        self.position = position
-        self.speed = speed
-        self.direction = direction
-        self.velocity = self.speed * self.direction
-        self.run_chance = run_chance
-        self.tumble_chance = tumble_chance
-        self.run_mode = run_mode
+def compute_mode_durations(mode_history, step_size):
+    """Calculate the durations of intervals of consecutive runs or tumbles."""
 
-        self.position_history = []
-        self.mode_history = np.array([],dtype=bool)
-    
-    def edge_check(self, box_size, step_size):
-        """Check if a cell has wandered to the edge of the box. If it has,
-        reverse its direction to simulate an elastic collision with the box
-        walls."""
+    count = 1
+    run_durations = np.array([])
+    tumble_durations = np.array([])
+    for i in range(1, len(mode_history)):
+        # Matching elements are part of the same interval
+        if mode_history[i] == mode_history[i-1]:
+            count += 1
+        # Differing elements indicates the start of a new interval
+        elif mode_history[i] != mode_history[i-1]:
+            time_interval = count * step_size
+            if mode_history[i-1] == True:
+                run_durations = np.append(run_durations,time_interval)
+            elif mode_history[i-1] == False:
+                tumble_durations = np.append(tumble_durations,time_interval)
+            count = 1
 
-        edge = 0.5*box_size - (self.speed * step_size)
-    
-        if abs(self.position[0]) >= edge:
-            self.direction[0] = -self.direction[0]
-                                                   
-        if abs(self.position[1]) >= edge:
-            self.direction[1] = -self.direction[1]
-
-        if abs(self.position[2]) >= edge:
-            self.direction[2] = -self.direction[2]
-
-    
-    def run(self, step_size):
-        """Move forwards in current direction"""
-        self.velocity = self.speed * self.direction
-        self.position = self.position + (self.velocity * step_size)
+    return run_durations, tumble_durations
 
 
-    def tumble(self, max_angle, x_matrix, y_matrix, z_matrix):
-        """Rotate randomly about the x, y and z axes"""
-        
-        R_x = x_matrix(np.random.random()*max_angle)
-        R_y = y_matrix(np.random.random()*max_angle)
-        R_z = z_matrix(np.random.random()*max_angle)
-        
-        self.direction = np.matmul(self.direction, R_x)
-        self.direction = np.matmul(self.direction, R_y)
-        self.direction = np.matmul(self.direction, R_z)
-        
-    
-    def switch_mode(self):
-        """Attempt to switch from run to tumble mode, and vice-versa"""
-        
-        random_float = np.random.random()
-        if self.run_mode==True:
-            if (random_float > self.run_chance) and (self.tumble_chance > 0):
-                self.run_mode = False
-                
-
-        elif self.run_mode==False:
-            if (random_float > self.tumble_chance) and (self.run_chance > 0):
-                self.run_mode = True
-               
-
-
-    def update(self):
-        """Attempt to switch movement mode then execute said mode. Append data
-        to arrays."""
-        
-        switch_success = self.switch_mode()
-
-        if self.run_mode == True:
-            #self.edge_check(System.box_size, System.step_size)
-            self.run(System.step_size)
-        elif self.run_mode == False:
-            self.tumble(2*System.pi,
-                    rotation_matrix_x, rotation_matrix_y, rotation_matrix_z)
-
-        self.position_history.append(np.copy(self.position))
-        self.mode_history = np.append(self.mode_history,self.run_mode)
-    
-
-
-def compute_mode_duration(mode_history, step_size):
-    pass
-
-swimmer = Cell(name='Escherichia coli', position=np.array([0,0,0]), speed=20,
-               direction=np.array([1,1,0]), run_chance=0.3, tumble_chance=0.1,
-               run_mode=True)
+swimmer = Cell3D(name='Escherichia coli', position=np.array([0,0,0]), speed=20,
+               direction=np.array([1,1,0]), run_chance=0.3, tumble_chance=0.1)
 
 
 for time in System.timesteps:   
-    swimmer.update()    
+    swimmer.update(System.step_size, System.pi, rotation_matrix_x,
+                   rotation_matrix_y, rotation_matrix_z)    
  
 positions = np.array(swimmer.position_history)
 x_pos = positions[:,0]
@@ -141,12 +80,18 @@ y_pos = positions[:,1]
 z_pos = positions[:,2]
 displacement = np.array(np.linalg.norm(positions,ord=None,axis=1))
 
+run_intervals, tumble_intervals = compute_mode_durations(swimmer.mode_history,
+                                                         System.step_size)
+
+print(run_intervals.size)
+print(tumble_intervals.size)
+
 # plot x,y
 plt.plot(x_pos,y_pos)
 plt.xlabel('x ($\mu$m)')
 plt.ylabel('y ($\mu$m)')
-plt.xlim(-System.box_size,System.box_size)
-plt.ylim(-System.box_size,System.box_size)
+plt.xlim(-0.5*System.box_size,0.5*System.box_size)
+plt.ylim(-0.5*System.box_size,0.5*System.box_size)
 plt.plot([-0.5*System.box_size,0.5*System.box_size],[0,0],color='k',ls='--',
          lw=0.5)
 plt.plot([0,0],[-0.5*System.box_size,0.5*System.box_size],color='k',ls='--',
@@ -177,4 +122,9 @@ plt.savefig('z_hist.png')
 plt.close()
 
 # plot durations of runs and tumbles
+plt.hist(run_intervals,bins='auto',color='r')
+plt.hist(tumble_intervals,bins='auto',color='b')
+plt.show()
+plt.savefig('intervals.png')
+plt.close()
 
