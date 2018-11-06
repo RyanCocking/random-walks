85086b894704316a077fea94f0d6a9efca2b8d50
RyanCocking <ry.ctbc@gmail.com>
2018-11-06 17:34:07 +0000

    Added simulation of multiple cells and trajectory plotting thereof.

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import numpy as np
 from cell_3d import Cell3D
 from cell_2d import Cell2D
 from cell_1d import Cell1D
+import plot
 
 class System:
     
-    max_time = 1e3  # seconds
+    total_cells = 5  # number of swimmers (non-interacting)
+    
+    ndim = 2  # dimensionality of the system
+
+    max_time = 1e2  # seconds
     step_size = 1
     total_steps = int(max_time / step_size)
     timesteps = np.linspace(0,max_time,num=total_steps)
@@ -19,19 +24,29 @@ class System:
     box_size = 800  # micrometres
 
 
-swimmer2D = Cell2D(name='Escherichia coli', position=np.array([0,0]), speed=20,
-               direction=np.array([1,1]), tumble_chance=0.1)
+# Create many swimming cells
+swimmers = []
+for i in range(System.total_cells):
+    swimmers.append(Cell2D(name='Escherichia coli', position=np.array([0,0]),
+        speed=20, direction=np.array([1,1]), tumble_chance=0.1))
 
+# Step through time
 for time in System.timesteps:   
-    swimmer2D.update(System.step_size, 2*np.pi)
- 
-positions = np.array(swimmer2D.position_history)
-x_pos = positions[:,0]
-y_pos = positions[:,1]
-displacements = np.array(np.linalg.norm(positions,ord=None,axis=1))
+    # Update every cell
+    for swimmer in swimmers:
+        swimmer.update(System.step_size, 2*np.pi)
 
-# plot x,y
-plt.plot(x_pos,y_pos)
+# Create list of cell trajectories
+positions = []
+for swimmer in swimmers:
+    positions.append(np.array(swimmer.position_history))
+
+# Plot trajectory of first cell
+print(len(positions))
+plot.trajectory(positions, System.box_size)
+
+quit()
+
 plt.xlabel('x ($\mu$m)')
 plt.ylabel('y ($\mu$m)')
 plt.xlim(-0.5*System.box_size,0.5*System.box_size)
@@ -40,26 +55,4 @@ plt.plot([-0.5*System.box_size,0.5*System.box_size],[0,0],color='k',ls='--',
          lw=0.5)
 plt.plot([0,0],[-0.5*System.box_size,0.5*System.box_size],color='k',ls='--',
          lw=0.5)
-plt.savefig('xy_traj.png')
-plt.close()
-
-# plot t,<r^2>
-plt.plot(System.timesteps, displacements)
-plt.ylabel('|r| ($\mu$m)')
-plt.xlabel('t (s)')
-plt.xlim(0,System.max_time)
-plt.savefig('norm_r_vs_t.png')
-plt.close()
-
-# plot x,y distributions
-plt.hist(np.unique(x_pos), bins='auto', density=False)
-plt.xlabel('x ($\mu$m)')
-plt.ylabel('Frequency')
-plt.savefig('x_hist.png')
-plt.close()
-
-plt.hist(np.unique(y_pos), bins='auto', density=False)
-plt.xlabel('y ($\mu$m)')
-plt.ylabel('Frequency')
-plt.savefig('y_hist.png')
-plt.close()
+plt.savefig('xy_trajectory.png')
--- plot.py
+++ plot.py
@@ -13,12 +13,51 @@ class FigureTemplate(matplotlib.figure.Figure):
 def distribution(xdata, dataname):
     """Probability distribution of a 1D dataset"""
     fig = plt.figure(FigureClass=FigureTemplate)
-    figname = 'dist_'+dataname
+    figname = 'distribution_'+dataname
 
     n, bins, patches = plt.hist(xdata, bins='auto')
 
     plt.savefig(figname+'.png')
     plt.close()
 
+def trajectory(traj_all_cells, box_size):
+    """Plot trajectories of some positional data for a single cell in 
+    1-3 dimensions. \'traj_all_cells\' is 1D list of length equal to the 
+    number of cells. Each element contains the trajectory of a cell, 
+    which may be in 1, 2 or 3 dimensions."""
 
-distribution(np.array([1,2,2,3,3,3,4,4,4]),'example')
+    fig = plt.figure(FigureClass=FigureTemplate)
+    figname = 'trajectory_'
+    ndim = traj_all_cells[0].shape[1]
+
+    if ndim == 1:
+        dataname = 'x'
+    elif ndim == 2:
+        dataname = 'xy'
+        legend_labels = []
+        for cell_number,traj in enumerate(traj_all_cells,1):
+            x = traj[:,0]
+            y = traj[:,1]
+            plt.plot(x,y)
+            legend_labels.append('Cell {}'.format(cell_number))
+
+        # Extra bits for clarity
+        plt.xlabel('x ($\mu$m)')
+        plt.ylabel('y ($\mu$m)')
+        plt.xlim(-0.5*box_size,0.5*box_size)
+        plt.ylim(-0.5*box_size,0.5*box_size)
+        plt.plot([-0.5*box_size,0.5*box_size],[0,0],color='k',ls='--',lw=0.5)
+        plt.plot([0,0],[-0.5*box_size,0.5*box_size],color='k',ls='--',lw=0.5)
+
+        plt.legend(legend_labels)
+
+    elif ndim == 3:
+        dataname = 'xyz'
+    else:
+        print("ERROR - Unacceptable number ({}) of dimensions in positional "
+                "data; should be 1, 2 or 3.".format(ndim))
+        quit()
+
+    figname += dataname
+    plt.savefig(figname+'.png')
+    plt.close()
