c88be19fefe6f1ce538433288b9c7acf3b17d1f5
Ryan Cocking <rctc500@york.ac.uk>
2018-11-15 13:52:35 +0000

    Added multi-plot trajectory figure for 3D.

--- figures.py
+++ figures.py
@@ -59,7 +59,7 @@ def trajectory(pos, box_size, title):
 
         x = pos[:,0]
         y = pos[:,1]
-        plt.plot(x,y,'-o',lw=0.5,ms=1.2)            
+        plt.plot(x,y,'-o',lw=0.5,ms=1.2)
 
         # Extra bits for clarity
         plt.xlabel('x ($\mu$m)')
@@ -69,6 +69,9 @@ def trajectory(pos, box_size, title):
         plt.plot([-0.5*box_size,0.5*box_size],[0,0],color='k',ls='--',lw=0.5)
         plt.plot([0,0],[-0.5*box_size,0.5*box_size],color='k',ls='--',lw=0.5)
 
+        plt.savefig(figname+dataname+'.png')
+        plt.close()
+
     elif ndim == 3:
         dataname = 'xyz'
 
@@ -76,30 +79,54 @@ def trajectory(pos, box_size, title):
         y = pos[:,1]
         z = pos[:,2]
 
-        # Subplots: static 3D plot alongside three planar projections in 2D
+        # NEEDS 
+        # combine 3d plot with 2d projections into a single figure
+        # 3d plot
+        ax3d = fig.add_subplot(221, projection='3d')
+        ax3d.set_xlabel('x ($\mu$m)')
+        ax3d.set_ylabel('y ($\mu$m)')
+        ax3d.set_zlabel('z ($\mu$m)')
+        ax3d.plot(x,y,z,'-o',lw=0.5,ms=1.2) 
+
+        # x,y projection
+        plt.subplot(222)
+        plt.plot(x,y)
+        plt.xlabel('x ($\mu$m)')
+        plt.ylabel('y ($\mu$m)')
+
+        # y,z projection
+        plt.subplot(223)
+        plt.plot(y,z)
+        plt.xlabel('y ($\mu$m)')
+        plt.ylabel('z ($\mu$m)')
+
+        # x,z projection
+        plt.subplot(224)
+        plt.plot(x,z)
+        plt.xlabel('x ($\mu$m)')
+        plt.ylabel('z ($\mu$m)')
+
+        plt.savefig(figname+dataname+'.png')
+        plt.close()
 
         # 3D rotating animation
-        ax = fig.add_subplot(111, projection='3d')
-        ax.set_title(title)
+        #ax = fig.add_subplot(111, projection='3d')
+        #ax.set_title(title)
 
-        ax.set_xlabel('x ($\mu$m)')
-        ax.set_ylabel('y ($\mu$m)')
-        ax.set_zlabel('z ($\mu$m)')
+        #ax.set_xlabel('x ($\mu$m)')
+        #ax.set_ylabel('y ($\mu$m)')
+        #ax.set_zlabel('z ($\mu$m)')
 
         # 3D point & line plot
-        ax.plot(x,y,z,'-o',lw=0.5,ms=1.2)
+        #ax.plot(x,y,z,'-o',lw=0.5,ms=1.2)
 
         # rotate the axes and update
-        for angle in range(0, 720):
-            ax.view_init(30, angle)
-            plt.draw()
-            plt.pause(.001)
+        #for angle in range(0, 720):
+        #    ax.view_init(30, angle)
+        #    plt.draw()
+        #    plt.pause(.001)
 
     else:
         print("ERROR - Unacceptable number ({}) of dimensions in positional "
                 "data; should be 2 or 3.".format(ndim))
         quit()
-
-    figname += dataname
-    plt.savefig(figname+'.png',dpi=400)
-    plt.close()
