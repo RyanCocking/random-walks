18e6c800d3b1a613236f80cbbad22938fbf5ee15
Ryan Cocking <rctc500@york.ac.uk>
2018-11-20 17:03:26 +0000

    Implemented Brownian motion (untested). Remember that setting a list equal to another list in python creates a REFERENCE to that list. To make a copy of a list, use np.copy()!

--- cell_3d.py
+++ cell_3d.py
@@ -28,27 +28,27 @@ class Cell3D:
     
     def __init__(self, name, position, speed, direction, tumble_chance):
         self.name = name
-        self.position = position
-        self.brownian_position = position
+        self.swim_position = position
+        self.brownian_position = np.copy(position)
         self.speed = speed
         self.direction = direction
         self.velocity = self.speed * self.direction
         self.tumble_chance = tumble_chance
 
-        self.position_history = []
-        self.position_history.append(np.copy(self.position))
+        self.swim_history = []
+        self.swim_history.append(np.copy(self.swim_position))
         self.brownian_history = []
-        self.brownian_history.append(np.copy(self.position))
+        self.brownian_history.append(np.copy(self.brownian_position))
 
     
-    def run(self, step_size):
-        """Move forwards in current direction"""
+    def run(self, time_step):
+        """Move forwards in current direction."""
         self.velocity = self.speed * self.direction
-        self.position = self.position + (self.velocity * step_size)
+        self.swim_position += (self.velocity * time_step)
 
    
     def tumble(self, max_angle):
-        """Rotate randomly about the x, y and z axes"""
+        """Rotate randomly about the x, y and z axesby some input angle."""
         
         R_x = rotation_matrix_x(np.random.random()*max_angle)
         R_y = rotation_matrix_y(np.random.random()*max_angle)
@@ -59,26 +59,44 @@ class Cell3D:
         self.direction = np.matmul(self.direction, R_z)
 
 
-    def trans_brownian_motion(self):
+    def trans_brownian_motion(self, bm_step):
         """Thermal fluctuations in the x, y and z axes. Uses the Berg
-        approach of having a 50% chance to move by +/- a step in each
+        approach of having a 50% chance to move +/- a step in each
         axis. Add the Brownian steps to their own position history."""
 
-        #self.brownian_position = self.brownian_position + 
-        pass
+        rng_x = np.random.random()
+        rng_y = np.random.random()
+        rng_z = np.random.random()
+
+        displacement = np.array([bm_step, bm_step, bm_step])
+
+        # swap signs if random numbers exceed 0.5
+        if rng_x > 0.5:
+            displacement[0] = -displacement[0]
+
+        if rng_y > 0.5:
+            displacement[1] = -displacement[1]
+
+        if rng_z > 0.5:
+            displacement[2] = -displacement[2]
+        
+        self.brownian_position += displacement
+
 
     def rot_brownian_motion(self):
+        """Under construction."""
         pass
 
-    def update(self, step_size, max_angle):
+    def update(self, bm_step, time_step, max_angle):
         """Execute a run and attempt to tumble every timestep. Append data
         to arrays."""
-         
-        #self.run(step_size)
+
+        self.trans_brownian_motion(bm_step)
+        #self.run(time_step)
 
         #Run-only
         #if np.random.random() > self.tumble_chance:
         #    self.tumble(max_angle)
 
-        self.position_history.append(np.copy(self.position))
         self.brownian_history.append(np.copy(self.brownian_position))
+        self.swim_history.append(np.copy(self.swim_position))
