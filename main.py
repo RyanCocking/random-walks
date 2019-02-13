2a78398477c39d3e075b09d0966d910484df5dfa
Ryan Cocking <rctc500@york.ac.uk>
2019-02-13 16:19:58 +0000

    Diffusion constants are now calculated from viscosity, temperature and cell radius.

--- main.py
+++ main.py
@@ -10,23 +10,31 @@ import figures as fg
 
 class System:
    
-    total_cells = 1  # non-interacting
-    box_size = 1000   # (micrometres (mu_m))
+    # Physical constants
+    boltz=1.38064852e-23  # J/k
+   
+    # System parameters
+    box_size = 1000    # mu_m
+    cell_radius = 1.0  # mu_m
+    temperature = 300  # K
+    viscosity = 0.01  # g/cm s (1 g/cm s = 1 Poise = 0.1 kg/m s)
+    
+    # Diffusion constants (Stokes-Einstein relation)
+    diffusion_constant = (boltz*temperature)/(6.0*np.pi*0.1*viscosity*1e-6*cell_radius)  # m^2/s
+    diffusion_constant *= 1e12   # mu_m^2/s
+    rot_diffusion_constant = (boltz*temperature)/(8.0*np.pi*0.1*viscosity*(1e-6*cell_radius)**3)  # rad^2/s
 
-    # time (s)
-    max_time = 34
-    time_step = 0.02
+    # Time
+    max_time = 34     # s
+    time_step = 0.02  # s
     total_steps = int(max_time / time_step)
-    timesteps = np.linspace(0,max_time,num=total_steps+1,
-            endpoint=True)  # includes t=0
+    timesteps = np.linspace(0, max_time, num=total_steps+1, endpoint=True)
 
-    diffusion_constant  = 1e-5  # water (cm^2 / s)
-    diffusion_constant *= 1e8   # (mu_m^2 / s)
-
-    # random number seed
+    # Random number seed
     seed = 98
     np.random.seed(seed)
 
+    # Graph header
     title = "Time = {}s, step size = {}s, seed = {}".format(max_time,
         time_step, seed)
 
@@ -54,10 +62,14 @@ class IO:
         return t, pos, pos_s
 
 
+print(System.diffusion_constant)
+print(System.rot_diffusion_constant)
+quit()
+
 # Instantiate cell classes
 print('Creating cells...')
 swimmers = []
-for i in range(System.total_cells):
+for i in range(1):
     swimmers.append(Cell3D(name='Escherichia coli', 
         position=np.array([0.0,0.0,0.0]), speed=20, 
         direction=np.array([1.0,0.0,0.0]), tumble_chance=0.1, 
