Fixed rotation test.

rotation_test.py
@@ -31,9 +31,6 @@ def inv_dot(a,b):
     magb=np.linalg.norm(b)
     return (1.0/(maga*magb))*np.arccos(np.dot(a,b))
 
-def todeg(angle):
-    return angle*180.0/3.14159
-
 #Setup 3d plot
 
 ax3d = plt.figure().add_subplot(111, projection='3d')
@@ -46,40 +43,72 @@ ax3d.plot([0,0],[0,.1],[0,0],'-',lw=1,color='k')
 ax3d.plot([0,0],[0,0],[0,.1],'-',lw=1,color='k')
 
 plt.tight_layout()
-plt.title('Final transformation')
 
-vec=np.array([1.0,1.0,1.0])
+vec=np.array([1,1,1])
 mag=np.linalg.norm(vec)
 rhat=vec/mag
+rhat_o=rhat
 
 x=np.array([1.0,0.0,0.0])
 y=np.array([0.0,1.0,0.0])
 z=np.array([0.0,0.0,1.0])
 
+# Original unit vector
 ax3d.plot([0,rhat[0]],[0,rhat[1]],[0,rhat[2]],'-',lw=3,color='r')
 ax3d.plot([rhat[0],rhat[0]],[rhat[1],rhat[1]],[rhat[2],0],'--',lw=2,color='r')
 ax3d.plot([rhat[0],0],[rhat[1],0],[0,0],'--',lw=2,color='r')
 
-print("Pre-transform")
-print("Rhat = ",rhat)
+print("Rhat1 = ",rhat)
 
 alpha=np.arccos(rhat[2]/1.0)
+alpha*=np.sign(rhat[0])
 beta=np.arctan(rhat[1]/rhat[0])
 
-print("Alpha = ",todeg(alpha))
-print("Beta = ",todeg(beta))
+print("Alpha1 = ",np.rad2deg(alpha))
+print("Beta1 = ",np.rad2deg(beta))
 print(" ")
 
-rhat=np.matmul(rhat,Rz(-beta))
-
+# First transform: -beta about z
+rhat=np.matmul(Rz(-beta),rhat)
 ax3d.plot([0,rhat[0]],[0,rhat[1]],[0,rhat[2]],'-',lw=3,color='b')
 ax3d.plot([rhat[0],rhat[0]],[rhat[1],rhat[1]],[rhat[2],0],'--',lw=2,color='b')
 ax3d.plot([rhat[0],0],[rhat[1],0],[0,0],'--',lw=2,color='b')
 
-rhat=np.matmul(rhat,Rx(-alpha))
-
+# Second transform: -alpha about y
+rhat=np.matmul(Ry(-alpha),rhat)
 ax3d.plot([0,rhat[0]],[0,rhat[1]],[0,rhat[2]],'-',lw=3,color='g')
 ax3d.plot([rhat[0],rhat[0]],[rhat[1],rhat[1]],[rhat[2],0],'--',lw=2,color='g')
 ax3d.plot([rhat[0],0],[rhat[1],0],[0,0],'--',lw=2,color='g')
 
+print("Rhat3 = ",rhat)
+print(" ")
+
+# TUMBLING
+tumble=np.deg2rad(10)
+rev=np.random.uniform(0,2*3.14159)
+print(np.rad2deg(rev))
+# Aligned vector, tumbled and revolved
+rhat=np.matmul(Ry(tumble),rhat)
+rhat=np.matmul(Rz(rev),rhat)
+ax3d.plot([0,rhat[0]],[0,rhat[1]],[0,rhat[2]],'-.',lw=3,color='g')
+ax3d.plot([rhat[0],rhat[0]],[rhat[1],rhat[1]],[rhat[2],0],'--',lw=2,color='g')
+ax3d.plot([rhat[0],0],[rhat[1],0],[0,0],'--',lw=2,color='g')
+
+# First back-transform: +alpha about y
+rhat=np.matmul(Ry(alpha),rhat)
+ax3d.plot([0,rhat[0]],[0,rhat[1]],[0,rhat[2]],'-.',lw=3,color='b')
+ax3d.plot([rhat[0],rhat[0]],[rhat[1],rhat[1]],[rhat[2],0],'--',lw=2,color='b')
+ax3d.plot([rhat[0],0],[rhat[1],0],[0,0],'--',lw=2,color='b')
+
+# Second back-transform: +beta about z
+rhat=np.matmul(Rz(beta),rhat)
+ax3d.plot([0,rhat[0]],[0,rhat[1]],[0,rhat[2]],'-.',lw=3,color='r')
+ax3d.plot([rhat[0],rhat[0]],[rhat[1],rhat[1]],[rhat[2],0],'--',lw=2,color='r')
+ax3d.plot([rhat[0],0],[rhat[1],0],[0,0],'--',lw=2,color='r')
+
+print("Tumble angle = ",np.rad2deg(tumble))
+print("Dot angle = ",np.rad2deg(np.arccos(np.dot(rhat,rhat_o))))
+
+ax3d.view_init(elev=50,azim=20)
+plt.savefig('Rotation.png')
 plt.show()